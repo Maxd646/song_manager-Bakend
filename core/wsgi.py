b2d14baeba77f4926bf274e50cf5ded1987d8a19
Daniel <ethiomiracle2017@gmail.com>
2025-07-22 13:11:08 +0300

    ,,,,

--- core/wsgi.py
+++ core/wsgi.py
@@ -1,4 +1,4 @@
 import os
 from django.core.wsgi import get_wsgi_application
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SONG_MANAGER_DJANGO_BACKEND.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 application = get_wsgi_application()
